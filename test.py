de4d38f3ac0478d476ac7d7efa5d028d7bd3cae8
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2023-09-19 02:16:44 +0300

    pu

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
+import unittest
+
 def add_numbers(a, b):
     return a + b
 
+class TestMathFunctions(unittest.TestCase):
+    def test_addition(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 5)
 
-def test_add_numbers():
-    assert add_numbers(2, 2) == 4  # This test case will succeed
-    assert add_numbers(3, 3) == 7  # This test case will fail
-
-
-if __name__ == "__main__":
-    try:
-        test_add_numbers()
-        print("All tests passed successfully!")
-    except AssertionError as e:
-        print("AssertionError:", str(e))
+if __name__ == '__main__':
+    unittest.main()

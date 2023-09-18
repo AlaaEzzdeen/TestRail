6201747ec70fb8266b68d762cf0cf12b8d4daa8e
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2023-09-19 00:58:33 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
 def add_numbers(a, b):
     return a + b
 
+
 def test_add_numbers():
     assert add_numbers(2, 2) == 4  # This test case will succeed
     assert add_numbers(3, 3) == 7  # This test case will fail
 
+
 if __name__ == "__main__":
-    def add_numbers(a, b):
-    return a + b
-
-def test_add_numbers():
-    assert add_numbers(2, 2) == 4  # This test case will succeed
-    assert add_numbers(3, 3) == 6  # This test case will succeed
-
     try:
         test_add_numbers()
         print("All tests passed successfully!")

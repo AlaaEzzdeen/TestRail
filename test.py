09c065d511a16270e878dc4ec55dd278aa806d66
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2023-09-19 00:57:18 +0300

    new settings

--- test.py
+++ test.py
@@ -6,4 +6,15 @@ def test_add_numbers():
     assert add_numbers(3, 3) == 7  # This test case will fail
 
 if __name__ == "__main__":
-    test_add_numbers()
+    def add_numbers(a, b):
+    return a + b
+
+def test_add_numbers():
+    assert add_numbers(2, 2) == 4  # This test case will succeed
+    assert add_numbers(3, 3) == 6  # This test case will succeed
+
+    try:
+        test_add_numbers()
+        print("All tests passed successfully!")
+    except AssertionError as e:
+        print("AssertionError:", str(e))

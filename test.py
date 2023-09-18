c0e5bf302798213bb3e8c556c5d65f9b3fa30792
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2023-09-19 02:24:26 +0300

    pu

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 import unittest
+import json
 
-def add_numbers(a, b):
-    return a + b
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        # Your test logic here
+        result = 42  # Replace with your actual test result
+        self.assertEqual(result, 42)  # Replace with your actual test assertion
 
-class TestMathFunctions(unittest.TestCase):
-    def test_addition(self):
-        result = add_numbers(2, 3)
-        self.assertEqual(result, 5)
+    def test_another_thing(self):
+        # Your test logic here
+        result = "success"  # Replace with your actual test result
+        self.assertEqual(result, "success")  # Replace with your actual test assertion
 
 if __name__ == '__main__':
-    unittest.main()
+    # Run the tests and save the results to a JSON file
+    test_results = unittest.main(exit=False).result.json_results()
+    with open('results.json', 'w') as file:
+        json.dump(test_results, file)
